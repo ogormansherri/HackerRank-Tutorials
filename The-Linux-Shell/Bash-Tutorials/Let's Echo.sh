c732c0f94cda1493363e562759f0dd7d8fe23009
Sherri Ann Verdugo <verdu103@mail.chapman.edu>
2015-01-21 09:50:09 -0800

    Update Let's Echo.sh

--- The-Linux-Shell/Bash-Tutorials/Let's Echo.sh
+++ The-Linux-Shell/Bash-Tutorials/Let's Echo.sh
@@ -1,2 +1,10 @@
+#Instructions
+##Problem Statement:Write a bash script which does just one thing: saying "HELLO".
+##Input Format: There is no input file required for this problem.
+##Output Format: HELLO
+##Sample Output: HELLO
+##Quick introduction to 'Echo' 
+###This is the equivalent of common output commands in most programming language (print or puts statements).
+
 #!/bin/bash
 echo "HELLO"
