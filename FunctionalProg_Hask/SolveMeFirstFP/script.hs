f6fa547c2623c548d8d96d74a7f2e03e0e345594
Sherri Ann Verdugo <verdu103@mail.chapman.edu>
2015-01-26 18:58:39 -0800

    Delete script.hs

--- FunctionalProg_Hask/SolveMeFirstFP/script.hs
+++
@@ -1,9 +0,0 @@
--- Platform: Haskell
-
-solveMeFirst a b = a + b --setting up function to sum two vars
-
-main = do 
-    val1 <- readLn --first value
-    val2 <- readLn --second value
-    let sum = solveMeFirst val1 val2 --obtain sum
-    print sum --show sum
